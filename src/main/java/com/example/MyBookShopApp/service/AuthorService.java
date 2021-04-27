919e663149a3adf1b54ff4657cb87cfa6cc281a8
Morozkin Vladislav <78484449+morozkin-vladislav@users.noreply.github.com>
2021-04-27 11:16:33 +0300

    HW4
    HW4

---
+++ src/main/java/com/example/MyBookShopApp/service/AuthorService.java
@@ -0,0 +1,30 @@
+package com.example.MyBookShopApp.service;
+
+import com.example.MyBookShopApp.data.Author;
+import com.example.MyBookShopApp.repository.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Service
+public class AuthorService {
+    private AuthorRepository authorRepository;
+
+    @Autowired
+    public AuthorService(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+
+    public Map<String, List<Author>> getAuthorsMap() {
+        List<Author> authors = authorRepository.findAll();
+
+        return authors.stream().collect(Collectors.groupingBy((Author a) -> {
+            return a.getLastName().substring(0, 1);
+        }));
+    }
+
+}
