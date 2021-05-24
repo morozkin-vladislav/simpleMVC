2dab649c91eb84e4e73d1b93b192504acb11819a
morozkin-vladislav <morozkin.vladislav@yandex.ru>
2021-05-24 14:05:14 +0300

    asd
    asd

--- src/main/java/com/example/MyBookShopApp/repository/BookRatingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.MyBookShopApp.repository;
-
-import com.example.MyBookShopApp.data.BookRating;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookRatingRepository extends JpaRepository<BookRating, Integer> {
-}
--- src/main/java/com/example/MyBookShopApp/repository/TestEntityCrudRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.MyBookShopApp.repository;
-
-import com.example.MyBookShopApp.data.TestEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TestEntityCrudRepository extends CrudRepository<TestEntity,Long> {
-}
